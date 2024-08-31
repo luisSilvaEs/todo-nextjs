1ee01c44f2a3d96194fb119565eb4b9323c8027c
Luis Silva <luissilva@Luiss-MacBook-Air.local>
2024-08-30 18:25:54 -0600

    Add up and down buttons to ToDo item

--- app/components/ToDo/TodoItem.tsx
+++ app/components/ToDo/TodoItem.tsx
@@ -1,3 +1,5 @@
+import { Button } from "./../Button";
+
 interface TodoItemProps {
   id: number;
   task: string;
@@ -7,6 +9,11 @@ const TodoItem = ({ id, task }: TodoItemProps) => {
   return (
     <li key={id} className="flex justify-between gap-x-6 py-5">
       <div className="flex min-w-0 gap-x-4 text-slate-100">
+        <input
+          id={`${id}`}
+          type="ckeckbox"
+          className="w-5 h-5 cursor-pointer peer"
+        />
         <label
           htmlFor={`${id}`}
           className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
@@ -14,12 +21,9 @@ const TodoItem = ({ id, task }: TodoItemProps) => {
           {task}
         </label>
       </div>
-      <div className="flex items-center">
-        <input
-          id={`${id}`}
-          type="ckeckbox"
-          className="w-5 h-5 cursor-pointer peer"
-        />
+      <div className="flex items-center gap-x-2">
+        <Button label="Move up" customClasses="arrow-up" />
+        <Button label="Move down" customClasses="arrow-down" />
       </div>
     </li>
   );
