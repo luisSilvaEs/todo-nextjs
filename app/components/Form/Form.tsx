95f43744ad1b7d923e8f177b474b73ce91022683
Luis Silva <luissilva@Luiss-MacBook-Air.local>
2024-08-31 12:40:01 -0600

    Create form component inluding new interface for component props. Mockup fields

--- app/components/Form/Form.tsx
+++ app/components/Form/Form.tsx
@@ -1,8 +1,40 @@
-const Form = () => {
+interface FormProps {
+  actionFunction?: () => void;
+  customClasses?: string;
+  fields: Array<{
+    label?: string;
+    type: string;
+    name: string;
+    customClasses: string;
+  }>;
+  buttons?: React.ReactNode[];
+}
+
+const Form = ({
+  actionFunction,
+  customClasses,
+  fields,
+  buttons,
+}: FormProps) => {
   return (
     <>
-      TODO FORM include Button, probably button shall be updated to include
-      submit tag
+      <form action={actionFunction} className={`${customClasses}`}>
+        {fields.map((Fld, _) => (
+          <>
+            {Fld.label && <label htmlFor={Fld.name}>{Fld.label}</label>}
+            <input
+              type={Fld.type}
+              name={Fld.name}
+              className={`${customClasses}`}
+            />
+          </>
+        ))}
+        <div className="flex gap-1 justify-end">
+          {/*buttons.map( Btn, _ ) => (
+
+            )*/}
+        </div>
+      </form>
     </>
   );
 };
--- app/newNote/page.tsx
+++ app/newNote/page.tsx
@@ -1,7 +1,19 @@
+import { Form } from "./../components/Form";
+
+const inputs = [
+  {
+    type: "text",
+    name: "title",
+    customClasses:
+      "border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100",
+  },
+];
+
 const Page = () => {
   return (
     <div>
       <h1>Create new note</h1>
+      {<Form customClasses="flex gap-2 flex-col" fields={inputs} />}
     </div>
   );
 };
